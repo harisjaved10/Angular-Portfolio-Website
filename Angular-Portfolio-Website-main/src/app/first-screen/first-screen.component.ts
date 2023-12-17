50c0ba49ca4f91afc15df73195fb2cf331738651
Haris <haris.javed@croeminc.com>
2023-12-17 08:45:07 +0500

    first commit

---
+++ Angular-Portfolio-Website-main/src/app/first-screen/first-screen.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from '../app.component';
+import { ResumeInformationComponent } from '../resume-information/resume-information.component';
+
+@Component({
+  selector: 'app-first-screen',
+  templateUrl: './first-screen.component.html',
+  styleUrl: './first-screen.component.css'
+})
+export class FirstScreenComponent  {
+
+}
